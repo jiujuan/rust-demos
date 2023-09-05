7432fe99d2e2559b52e668f4aaea818bdb70577c
九卷 <jiujuanfeng@163.com>
2023-09-05 22:35:00 +0800

    cargo main.rs

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
+    greet_world();
+}
+
+fn greet_world() {
+    let southern_germany = "Grüß Gott!";
+    let chinese = "世界，你好";
+    let english = "hello,world!";
+    let regions = [southern_germany, chinese, english];
+
+    for region in regions.iter() {
+        println!("{}", &region)
+    }
 }
